3fd1a07e8570b759b7750f8fdd1448d33c466e22
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-03-31 16:57:08 +0200

    Add general_contener content -> Date_calendar & table_options

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+
+//datepicker
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  // eslint-disable-next-line no-unused-vars
+  }, function(start, end, label) {
+    console.log('A new date selection was made: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+  
